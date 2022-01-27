2d226c34d25d07822a331551a44954663d083ea7
Rafael Santos <rafael.escaleira@hotmail.com>
2022-01-27 16:19:04 -0400

    Initial Commit

---
+++ Smart Client Fit/Shared/Smart_Client_FitApp.swift
@@ -0,0 +1,20 @@
+//
+//  Smart_Client_FitApp.swift
+//  Shared
+//
+//  Created by Rafael Escaleira on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Client_FitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
